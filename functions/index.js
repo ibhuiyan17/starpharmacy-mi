08ceb6628b07168680b25b17f3a45eaaa7be0457
ibhuiyan <ibhuiyan@umich.edu>
2021-08-19 22:08:37 -0400

    Working backend email cloud function

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,3 @@
-// const functions = require("firebase-functions");
 const functions = require("firebase-functions");
 const express = require("express");
 const sgMail = require("@sendgrid/mail");
@@ -8,40 +7,46 @@ const cors = require("cors");
 const app = express();
 app.use(cors({origin: true, credentials: true}));
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// constants
+const destinationEmail = functions.config().sendgrid.destination_email;
+const verfiedSenderEmail = functions.config().sendgrid.verfied_sender_email;
 
-app.get("/sayhi", (req, res) => {
-  console.log("hello!");
+app.get("/sayhi", (_, res) => {
+  console.log("Request to /sayhi endpoint received. Hello!");
   res.status(200).send("success");
 });
 
-app.post("/sendmail", (req, res) => {
+app.post("/sendemail", (req, res) => {
   sgMail.setApiKey(functions.config().sendgrid.apikey);
-  // eslint-disable-next-line max-len
 
-  const msg = {
-    to: "ibtidab@gmail.com", // Change to your recipient
-    from: "starpharmacymi.email@gmail.com", // Change to your verified sender
-    subject: "Testing",
-    text: "This is a test of the email feature.",
-    // html: "This is a test of the email feature.</strong>",
-  };
-  sgMail.send(msg)
+  const {
+    fullname,
+    email,
+    subject,
+    message,
+  } = JSON.parse(req.body);
+  sgMail.send(constructMsgObj(fullname, email, subject, message))
       .then(() => {
-        console.log("Email sent");
+        console.log(`Email on behalf of ${email} sent successfully`);
         res.status(200).send("success");
       })
       .catch((error) => {
+        console.error(`Failure to send email on behalf of ${email}`);
         console.error(error);
         console.error(error.response.body.errors);
         res.status(400).send("fail");
       });
 });
+
+const constructMsgObj = (fullname, email, subject, message) => {
+  const msgObj = {
+    reply_to: email,
+    to: destinationEmail,
+    from: verfiedSenderEmail,
+    subject: `Customer Inquiry from ${fullname}: ${subject}`,
+    text: message,
+  };
+  return msgObj;
+};
 
 exports.api = functions.https.onRequest(app);
